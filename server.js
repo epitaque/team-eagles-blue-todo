13971e080fee10be091ca30a9178ef31e1a692ab
Justin Chang <justinchangmusic@gmail.com>
2016-06-30 00:10:21 -0400

    Added/fixed API routes, changed Server.js
    I added a new User Model, and User API. Changed Todo Model to use User Model. Now we can return a list of users via '/users' and can make new accounts via '/signup' and login via '/login'

---
+++ server.js
@@ -0,0 +1,29 @@
+var express  = require('express');
+var mongoose = require('mongoose');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var ip       = process.env.IP;
+
+// Mongoose Config
+mongoose.connect('mongodb://jchang4:blueEagle@ds017193.mlab.com:17193/node-todo-demo');
+
+// Express Config
+app.set('view engine', 'ejs');
+
+// Import API and Models
+var UserApi = require('./api/UserApi');
+var TodoApi = require('./api/TodoAPI');
+var Todo = require('./models/TodoModel');
+// Initialize APIs
+UserApi(app);
+TodoApi(app);
+
+// Seed Database
+var seedDB = require('./seedDB')
+// seedDB(app);    // comment out so I dont run it every time server starts
+
+app.get('*', (req, res) => {
+    res.send("You've reached the 'Other' Route! Congrats! Now go back.");
+});
+
+app.listen(port);
