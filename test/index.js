5ac5d1cd6af6a009e8168f1626d1c1de516d111e
Johan Oakes <oakejp12@wfu.edu>
2016-06-30 20:54:10 -0400

    Complete first route test

--- test/index.js
+++ test/index.js
@@ -1,24 +1,43 @@
 var should = require('should');
+var request = require('supertest');
 var assert = require('assert');
 var mongoose = require('mongoose');
 var config = require('../lib/config');
 
 describe('routing', () => {
+  var server;
 
   // Within before() you can run all the operations
   // that are needed to setup the tests.
-  before(done => {
-    // Create a connection to the database
-    mongoose.connect(config.db.mongodb);
-    done();
-  });
 
   /*
-  describe('Account', () => {
-    it('should return error if user')
+  before(done => {
+    server = require('../lib/server');
+
+    // Create a connection to the database
+    // mongoose.connect(config.db.mongodb);
+    done();
   });
   */
 
+
+  beforeEach(() => server = require('../lib/server'));
+  afterEach(() => server.close());
+
+  describe('GET /users', () => {
+    it('should respond with json', (done) => {
+      request(server)
+        .get('/users')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err);
+          done();
+        });
+    });
+  });
+
+  
 
 
 });
