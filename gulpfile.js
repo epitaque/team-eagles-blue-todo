f334e59146c9ec71d6025d29c0c0d145c17c0267
John Burnette <burnette.john@gmail.com>
2016-07-07 21:15:42 -0400

    Fixed bad html from merge, preliminary styling for main view

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,14 @@ var gulp = require('gulp'),
 var files = ['client/styles/*.scss', 'node_modules/materialize-css/sass/materialize.scss'];
 
 gulp.task('build-sass', function() {
-  var tasks = files.map(function(element) {
-    return gulp.src(element)
-      .pipe(sass())
-      .pipe(cssmin())
-      .pipe(rename({
-        suffix: '.min'
-      }))
-      .pipe(gulp.dest('client/styles'));
-  })
-});
 
-gulp.task('copy-fonts', function() {
-  gulp.src('node_modules/materialize-css/dist/font/**/*.{ttf,woff,woff2,eof,svg}')
-    .pipe(gulp.dest('client/font/'));
-});
+  return gulp.src('client/styles/*.scss')
+    .pipe(sass())
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('client/styles'));
 
-gulp.task('default', ['build-sass', 'copy-fonts']);
+});
+gulp.task('default', ['build-sass']);
