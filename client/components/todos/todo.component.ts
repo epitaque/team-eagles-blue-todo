1d27b54d6205a7de9cd66ec3723160ccf280aac3
Brian Hoy <brianhoy4@gmail.com>
2016-07-08 11:25:41 -0400

    Fixing the styles of the todolist

--- client/components/todos/todo.component.ts
+++ client/components/todos/todo.component.ts
@@ -3,25 +3,26 @@ import {TodoService} from '../../services/todo.service';
 import {Todo} from '../../models/TodoModel';
 
 @Component({
+    host: {
+        'style': 'width: 100%'
+    },
     selector: 'todo',
     inputs: ['todo'],
-    template: `<div [style.background-color]="todo.completed ? 'gray' : 'transparent'">
-                    {{todo.task}}
-                    {{todo.importance}}
-                    <button (click)="setImportance(1)">Low Importance</button>
-                    <button (click)="setImportance(2)">Moderate Importance</button>
-                    <button (click)="setImportance(3)">High Importance</button>
+    outputs: ['todo'],
+    template: `<div class="container" style="width: 100%" [style.background-color]="todo.completed ? 'gray' : 'transparent'">
+                    <div class="g--1 no-margin-vertical">
+                        <input class="g--1 no-margin-vertical" type="checkbox" [(ngModel)]="todo.completed">
+                    </div>
+                    <p class="g--6 no-margin-vertical"> {{todo.task}} </p>
+                    <select class="g--5 no-margin-vertical" [(ngModel)]="todo.importance">
+                        <option [selected]="importance == 1">Low Importance</option>
+                        <option [selected]="importance == 2">Medium Importance</option>
+                        <option [selected]="importance == 3">High Importance</option>
+                    </select>
                 </div>
     `
 })
 export class TodoComponent {
     private todo: Todo;
-    private importance: number;
-    constructor() {
-    }
-
-    private setImportance(importance: number): void {
-        this.importance = importance;
-        this.todo.importance = importance;
-    }
+    constructor() {}
 }
